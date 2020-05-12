c84c4bc289fc91b347779664e50d6e7bdedfa6c4
soramin <44796681+soramin@users.noreply.github.com>
2020-05-12 19:38:02 +0900

    Add files via upload

---
+++ Rectangle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test5_1
+{
+    class Rectangle
+    {
+        private int height = new int();
+        private int width = new int();
+
+        private int Height
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                this.height = (value < 10 ? 10 : value);
+            }
+        }
+
+        private int Width
+        {
+            get
+            {
+                return width;
+            }
+            set
+            {
+                this.width = (value < 10 ? 10 : value);
+            }
+        }
+
+        public int Menseki()
+        {
+           height = 3;
+           width = 3;
+
+            return height * width;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("$長方形の面積は{Menseki}です。");
+        }
+    }
+}
